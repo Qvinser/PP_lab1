afc88ff76a43ad44773d66d0a6b43dc4945ca56c
Qvinser <ribnayastaya777@gmail.com>
2025-02-27 16:55:52 +0700

    fix 10038

--- PP_lab1_HTTP/Source.cpp
+++ PP_lab1_HTTP/Source.cpp
@@ -7,13 +7,11 @@
 #include <sys/types.h> 
 #include <pthread.h>
 using namespace std;
-#pragma comment(lib, "Ws2_32.lib")
+#pragma comment (lib, "wsock32.lib")
 
 char response1[] =
 "HTTP/1.1 200 OK\n"
-"Content-Type: text/html; charset=UTF-8"
-"Cache-Control: private, max-age=0"
-"Connection: keep-alive\r\n\r\n"
+"Content-Type: text/html; charset=UTF-8\r\n\r\n"
 "<!DOCTYPE html><html><head><title>Bye-bye baby bye-bye</title>"
 "<style>body { background-color: #111 }"
 "h1 { font-size:2cm; text-align: center; color: white;"
@@ -24,21 +22,38 @@ static int request_number = 0;
 
 void* thread_job(void* arg)
 {
-    int* client_fd = (int*)arg;
-    printf("got connection\n");
+    int client_fd = (int)arg;
+    //printf("got connection\n");
 
-    if (*client_fd == -1) {
+    if (client_fd == -1) {
         perror("Can't accept");
         return 0;
     }
     request_number += 1;
-    send(*client_fd, response1, sizeof(response1)-1, NULL); /*-1:'\0'*/
+    int iResult = send(client_fd, response1, sizeof(response1)-1, NULL); /*-1:'\0'*/
+    if (iResult == SOCKET_ERROR) {
+        wprintf(L"send failed with error: %d, socket: %d\n", WSAGetLastError(), client_fd);
+        closesocket(client_fd);
+        return NULL;
+    }
     std::string s = std::to_string(request_number);
-    char const* pchar = s.c_str();
-    send(*client_fd, pchar, sizeof(pchar)-1, NULL); /*-1:'\0'*/
-    send(*client_fd, response2, sizeof(response2)-1, NULL); /*-1:'\0'*/
-    Sleep(2);
-    closesocket(*client_fd);
+    const char* pchar = s.c_str();
+    iResult = send(client_fd, pchar, sizeof(pchar)-1, NULL); /*-1:'\0'*/
+    if (iResult == SOCKET_ERROR) {
+        wprintf(L"send failed with error: %d, socket: %d\n", WSAGetLastError(), client_fd);
+        closesocket(client_fd);
+        return NULL;
+    }
+    iResult = send(client_fd, response2, sizeof(response2)-1, NULL); /*-1:'\0'*/
+    if (iResult == SOCKET_ERROR) {
+        wprintf(L"send failed with error: %d, socket: %d\n", WSAGetLastError(), client_fd);
+        closesocket(client_fd);
+        return NULL;
+    }
+    //Sleep(5);
+    s.clear();
+    shutdown(client_fd, SD_SEND);
+    closesocket(client_fd);
 }
 
 void err(int code, const char buff[]) {
@@ -49,7 +64,7 @@ void err(int code, const char buff[]) {
 int main()
 {
     int retval;
-    BOOL bOptVal = TRUE, client_fd;
+    BOOL bOptVal = TRUE;
     int bOptLen = sizeof(BOOL);
     struct sockaddr_in svr_addr, cli_addr;
     int sin_len = sizeof(cli_addr);
@@ -82,11 +97,15 @@ int main()
     listen(sock, 5);
     cout << "Listening..." << endl;
     while (1) {
-        client_fd = accept(sock, (struct sockaddr*)&cli_addr, &sin_len);
+        int client_fd = accept(sock, (struct sockaddr*)&cli_addr, &sin_len);
+        if (client_fd == INVALID_SOCKET) {
+            wprintf(L"accept failed with error: %ld\n", WSAGetLastError());
+            continue;
+        }
 
         // Создание потока
         pthread_t threads;
-        retval = pthread_create(&threads, NULL, thread_job, &client_fd);
+        retval = pthread_create(&threads, NULL, thread_job, (int*)client_fd);
         // Если при создании потока произошла ошибка, выводим
         // сообщение об ошибке и прекращаем работу программы
         if (retval != 0) {
